db8cd28363086b00612bf1def757656c5da2c2c6
fantasyui-com <captain@fantasyui.com>
2019-08-10 15:57:14 -0400

    Code Sync

--- test.mjs
+++ test.mjs
@@ -1,18 +1,95 @@
 #!/usr/bin/env node --experimental-modules --no-warnings
+//
+
+//
 
 import und from './index.mjs';
 
-(async function main(){
+import assert from 'assert';
 
-  const {get, set} = await und({db:'./my-db'})
-  console.log({get, set})
-  let doc = await get('spot-1');
+async function test(label, fun){
 
-  if(doc){
-    // all is well
-  }else{
-    doc = set( {id:'spot-1', name: 'Hello World!'} )
+  try{
+    await fun(assert)
+    console.info(`\nPASS: ${label}`)
+  }catch(e){
+    console.error(`\nFAIL: ${label}`)
+    console.log(e);
+
   }
 
+}
 
-})()
+
+(async function main(){
+
+  const {
+    get, set, del,
+
+    upsert,
+    conflicts,
+
+    erase,
+    clean,
+    cleanup,
+
+    sync,
+    rsync,
+
+    view,
+
+
+  } = await und({db:'./my-db'});
+
+  test('Get Missing Document', async test => {
+    let result = await get('mallory');
+  	test.equal(result, null);
+  });
+
+  test('Get Existing Document', async test => {
+    let {meta, data} = await get('alice');
+  	test.equal(meta.id, 'alice');
+  });
+
+  test('Upsert a Document', async test => {
+  	let doc = await upsert('alice', {name: 'Hello World!'});
+  	test.equal(meta.id, 'alice');
+  });
+
+  test('Get Conflicts', async test => {
+  	let conflicts = await conflicts('alice');
+  	test.equal(meta.id, []);
+  });
+
+  test('Create a View', async test => {
+  	let view = await map('list-administrators',
+      function(doc){
+
+        if(doc.data.administrator){
+          if(doc.meta.deleted === false){
+            return doc;
+          }
+        }
+
+      }
+    );
+
+  	let view = await reduce('count-administrators', 'list-administrators'
+      function(accumulator, doc){
+        // for everytime a doc is emited
+
+        // check if valid
+        if(doc.meta.deleted === true){
+          accumulator.count--;
+        }else{
+          accumulator.count++;
+        }
+        // increment
+
+      },{count:0}
+    );
+
+  	test.equal(meta.id, []);
+  });
+
+})();
